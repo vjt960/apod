c1f4943afd900d5d06fff9fb3ae6afd7b5971718
Vinton Teo <taulago@ymail.com>
2019-09-17 15:20:27 -0600

    Fix month variable to add 1 to method instead of subtract; remove console logs

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,12 +1,14 @@
 const apiKey = process.env.VUE_APP_API_KEY;
 
 export const getAPOD = async selectedDate => {
+  console.log(process.env);
   const url = 'https://api.nasa.gov/planetary/apod?';
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth() - 1;
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   const timeStamp = selectedDate ? selectedDate : `${year}-${month}-${day}`;
+  console.log(year, month, day);
   try {
     const response = await fetch(
       `${url}api_key=${apiKey}&date=${timeStamp}&hd=false`
