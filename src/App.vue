92f3597f317805a0bf9ec80ae650e6e1e3ef8629
Vinton Teo <taulago@ymail.com>
2019-09-17 13:41:08 -0600

    Conditionally render img and iframe based on apod media_type

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,56 @@
-<template> 
-  <div id="app"> 
-    <h1 @click='clickFn' >NASA's Astronomy Picture of the Day</h1> 
-    <p>{{this.timesClicked}}</p>
-    <img class='apod' :alt=this.title :src=this.apod />
-    <p class='apod-description'>{{this.desc}}</p>
-  </div>
+<template>
+	<div id="app">
+		<h1 @click="clickFn">NASA's Astronomy Picture of the Day</h1>
+		<p>{{this.timesClicked}}</p>
+		<img v-if="this.mediaType === 'image'" class="apod" :alt="this.title" :src="this.apod" />
+		<iframe v-else type="text/css" height="500" width="720" :title="this.title" :src="this.apod"></iframe>
+		<p class="apod-description">{{this.desc}}</p>
+	</div>
 </template>
 
 <script>
-import { getAPOD } from './utils/apiCalls';
+	import { getAPOD } from "./utils/apiCalls";
 
-export default {
-  name: 'app', 
-  data() {
-    return {
-      apod: '', 
-      title: '', 
-      desc: '', 
-      timesClicked: 0
-    }
-  }, 
-  async mounted() {
-    const data = await getAPOD(); 
-    this.apod = data.url; 
-    this.desc = data.explanation; 
-    this.title = data.title;
-  }, 
-  methods: {
-    clickFn() {
-      this.timesClicked ++;
-    }, 
-    async changeApodByDate(date) {
-      const data = await getAPOD(date); 
-      this.apod = data.url; 
-      this.desc = data.explanation; 
-      this.title = data.title;
-    }
-  }
-}
+	export default {
+		name: "app",
+		data() {
+			return {
+				apod: "",
+				title: "",
+				desc: "",
+				mediaType: ""
+			};
+		},
+		async mounted() {
+			const data = await getAPOD();
+			this.apod = data.url;
+			this.desc = data.explanation;
+			this.title = data.title;
+			this.medaType = data.media_type;
+		},
+		methods: {
+			async changeApodByDate(date) {
+				const data = await getAPOD(date);
+				this.apod = data.url;
+				this.desc = data.explanation;
+				this.title = data.title;
+			}
+		}
+	};
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} 
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 
-h1:hover {
-  cursor: pointer; 
-  color: red;
-}
+	h1:hover {
+		cursor: pointer;
+		color: red;
+	}
 </style>
