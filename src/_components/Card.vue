d1b3a6e12411e0a7e3dc47643f5570710dacc938
Vinton Teo <taulago@ymail.com>
2019-09-18 00:14:44 -0600

    Create Card component to render individual apod from Calendar component

---
+++ src/_components/Card.vue
@@ -0,0 +1,40 @@
+<template functional>
+	<article>
+		<iframe class="calendar-vid" v-if="props.media_type === 'video'" :src="props.apod"></iframe>
+		<img class="calendar-img" v-else :src="props.apod" :alt="props.title" />
+	</article>
+</template> 
+
+<script>
+	export default {
+		name: "Card",
+		props: {
+			apod: {
+				required: true,
+				type: String
+			},
+			media_type: {
+				required: true,
+				type: String
+			},
+			title: {
+				required: true,
+				type: String
+			}
+		}
+	};
+</script> 
+
+<style scoped>
+	.article {
+		height: 275px;
+		width: 200px;
+	}
+
+	iframe,
+	img {
+		object-fit: cover;
+		height: 255px;
+		width: 100%;
+	}
+</style>
