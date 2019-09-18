dfac39f433c316bb16e0fbb53e6e444add24b3fe
Vinton Teo <taulago@ymail.com>
2019-09-18 00:11:15 -0600

    Create function to fetch all apods from current month

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -7,11 +7,26 @@ export const getAPOD = async selectedDate => {
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const timeStamp = selectedDate ? selectedDate : `${year}-${month}-${day}`;
-  console.log(year, month, day);
   try {
     const response = await fetch(
       `${url}api_key=${apiKey}&date=${timeStamp}&hd=false`
     );
+    return response.json();
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+export const getMonthsAPOD = async () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const startDate = `${year}-${month}-1`;
+  const timeStamp = `${year}-${month}-${day}`;
+  const fullMonthUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${startDate}&end_date=${timeStamp}`;
+  try {
+    const response = await fetch(fullMonthUrl);
     return response.json();
   } catch (error) {
     throw Error(error.message);
